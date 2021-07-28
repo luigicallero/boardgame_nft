6c36fdb022c1fd3d3100204293a70f9f39220938
Luisca <luigicallero@gmail.com>
2021-07-28 08:54:18 -0300

    generation completed now starting with nft skelethon

--- contracts/BoardGame.sol
+++ contracts/BoardGame.sol
@@ -7,18 +7,18 @@ pragma solidity >=0.7.0 <0.9.0;
  * @dev Play a Simple Boardgame with predisigned NFTs
  */
 contract BoardGame {
-
+// pending the definition of each head & eyes part. e.g.: heads has 10 types of heads: head1=blond, head2=bold, etc
     uint256 heads = 10;
     uint256 eyes = 3;
     uint256 headId;
     uint256 eyeId;
     uint256 finalId;
-    uint id = 0; //temporarily used as counter of figures and ID
     struct Figure {
-        uint id;
         uint figureId;
+        bool minted;
     }
     
+    uint [] public array;
     Figure[] public figures;
     
     /**
@@ -30,13 +30,27 @@ contract BoardGame {
         // Generating the combination of avatars using 10 heads & 3 eyes
         for (uint h=0; h < heads; h++){
             for (uint e=0; e < eyes; e++){
-                id ++;
-                figures.push(Figure(id, h * 100 + e));
+                figures.push(Figure(h * 100 + e, false));
             }
         }
     }
     
-    function retrievesAlgorythm(uint _index) public view returns (uint256){
+    function retrievesFigureId(uint _index) public view returns (uint256){
         return figures[_index].figureId;
     }
+    
+    /**
+     * We will user randomnes with Chainlink 
+    
+    function selector() public view returns (uint256){
+        uint mintFaceId = uint(keccak256(abi.encodePacked(msg.sender))) % 1000; // modulus here is to limit the amount of digits generated to the amount of array ids
+        // uint arrayId = uint(figures[mintFaceId]);
+        if (array[0]){
+            
+        }
+        while( figures[mintFaceId] == 5 ) {
+            return mintFaceId;
+        }
+    }
+    **/
 }
